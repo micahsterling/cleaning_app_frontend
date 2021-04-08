e4adbcde297d01082073f9bfa4e345327ba98a14
Micah Sterling <msterling89@gmail.com>
2021-04-08 18:49:47 -0500

    add posts index

---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="posts">
+    <h1>{{ message }}</h1>
+    <div v-for="post in posts">
+      {{post.title}}
+      {{post.votes}}
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Posts!",
+      posts: [],
+    };
+  },
+  created: function () {
+    console.log("creating");
+    this.postsIndex();
+  },
+  methods: {
+    postsIndex: function () {
+      console.log("posts index");
+      // posts web request
+      axios.get("/api/posts").then((response) => {
+        console.log(response.data);
+        this.posts = response.data;
+      });
+    },
+  },
+};
+</script>
