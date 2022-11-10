e7b40886afca42095dbccd0e55c819530b5dfd35
Micah Sterling <msterling89@gmail.com>
2022-11-10 16:50:05 -0600

    update backend

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
-import { MultiSelectPlugin } from '@syncfusion/ej2-vue-dropdowns';
+import { MultiSelectPlugin } from "@syncfusion/ej2-vue-dropdowns";
 
 Vue.use(MultiSelectPlugin);
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "http://localhost:3000" : "https://enigmatic-sands-74548.herokuapp.com";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://cleaning-app.up.railway.app/"
+    : "http://localhost:3000";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
@@ -17,5 +20,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
